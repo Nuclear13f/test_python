07654eeb1ea27a56663de9da3ec027b36163334b
Master <nuclear13f@gmail.com>
2024-06-27 15:40:30 +0300

    ver 3

---
+++ auth.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user, logout_user
+from model import users
+
+
+
+
+auth = Blueprint('auth', __name__)
+
+
+
+@auth.route('/', methods=['GET', 'POST'])
+@login_required #входят только зарегистрированные пользователи
+def home():
+    print('home')
+    return render_template("home.html", user=current_user)
+
+
+
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        user = users.query.filter_by(login='Sergey').first()
+        print(password)
+        print(user.password)
+        if user.password == password:
+            print('Есть такой юзер')
+
+
+
+    return render_template('login.html', user=current_user)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    print('un reg')
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    x = 5
-    print(x)
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from __init__ import create_app
+from flask_bcrypt import Bcrypt
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
